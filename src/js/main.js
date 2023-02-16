a8d00f10762d778a9020d47e6d50ccef544b8fb4
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-02-16 16:18:55 +0100

    Add navigation for mobile devices

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
-const headerVideo = document.querySelector('.header__bg-video');
+const headerVideoEl = document.querySelector('.header__video-c video');
+headerVideoEl.playbackRate = 0.8;
 
-headerVideo.playbackRate = 0.8;
+const hamburgerEl = document.querySelector('.hamburger');
+const navbarMenuEl = document.querySelector('.navbar__menu');
+
+const mobileMenuHandle = function () {
+	navbarMenuEl.classList.toggle('navbar__menu--active');
+};
+
+hamburgerEl.addEventListener('click', mobileMenuHandle);

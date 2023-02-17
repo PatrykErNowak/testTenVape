b85339ecaefeb3648168315f51ae73e0ce822c59
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-02-17 16:03:51 +0100

    Add animations for mobile navigation & Fix header responsivness

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
+// Video
 const headerVideoEl = document.querySelector('.header__video-c video');
 headerVideoEl.playbackRate = 0.8;
 
+// ---------------------------------
+// Navigation handling
 const hamburgerEl = document.querySelector('.hamburger');
 const navbarMenuEl = document.querySelector('.navbar__menu');
 
 const mobileMenuHandle = function () {
 	navbarMenuEl.classList.toggle('navbar__menu--active');
+	hamburgerEl.classList.toggle('is-active');
 };
 
 hamburgerEl.addEventListener('click', mobileMenuHandle);

0f899ffb6af90528944e4313caef68d4154befac
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-02-26 18:01:12 +0100

    Small Fix repair JS error in FIND-US page

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 // Video
 const headerVideoEl = document.querySelector('.header__video-c video');
 
-headerVideoEl.playbackRate = 0.8;
+if (headerVideoEl) {
+	headerVideoEl.playbackRate = 0.8;
 
-// Change video quality depends of screen size
-const videoQuality = window.screen.height > 1080 ? '1080' : window.screen.height > 720 ? '720' : '';
-headerVideoEl.src = `./src/video/header-${videoQuality}p.mp4`;
+	// Change video quality depends of screen size
+	const videoQuality = window.screen.height > 1080 ? '1080' : window.screen.height > 720 ? '720' : '';
+	headerVideoEl.src = `./src/video/header-${videoQuality}p.mp4`;
+}
 
 // ---------------------------------
 // Navigation handling

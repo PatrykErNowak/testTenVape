297ef99aa47a6fd7f5048a1a760a1dfe4d4b5d65
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-03-03 18:08:42 +0100

    Fix categories section design in OFFER page

--- src/js/pages/offer.js
+++ src/js/pages/offer.js
@@ -1,27 +1,31 @@
-const imageEl = document.querySelector('.category__img');
+const categoryItems = document.querySelectorAll('.category');
 
-imageEl.addEventListener('mousemove', (e) => {
+const rotate3dElement = function (e, target, maxDegrees, scale) {
 	const mouse = {
 		x: e.layerX,
 		y: e.layerY,
 	};
 	const image = {
-		x: imageEl.scrollWidth,
-		y: imageEl.scrollHeight,
+		x: target.scrollWidth,
+		y: target.scrollHeight,
 	};
 
 	const calcRotateDEG = (mouseAxis, elAxisDimension, maxDegree) => {
 		return ((mouseAxis / elAxisDimension) * 2 - 1) * maxDegree;
 	};
 
-	imageEl.style.transform = `perspective(800px) rotateX(${calcRotateDEG(
+	target.style.transform = `perspective(800px) rotateX(${calcRotateDEG(
 		mouse.y,
 		image.y,
-		15
-	)}deg) rotateY(${-calcRotateDEG(mouse.x, image.x, 15)}deg) scale3d(1.03, 1.03, 1.03)`;
-});
+		maxDegrees
+	)}deg) rotateY(${-calcRotateDEG(mouse.x, image.x, maxDegrees)}deg) scale3d(${scale}, ${scale}, ${scale})`;
+};
 
-// Restore position of element when mouse out of the element
-imageEl.addEventListener('mouseout', () => {
-	imageEl.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)';
-});
+// categoryItems.forEach(function (cat) {
+// 	cat.addEventListener('mousemove', function (e) {
+// 		rotate3dElement(e, cat, 10, 1.02);
+// 	});
+// 	cat.addEventListener('mouseout', () => {
+// 		cat.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)';
+// 	});
+// });

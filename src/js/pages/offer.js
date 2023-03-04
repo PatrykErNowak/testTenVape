d5284a9e64b04b2e20ac1ad69d56cc4e7c9fb36d
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-03-04 19:23:00 +0100

    Fix categories section design and behavior for mobile devices

--- src/js/pages/offer.js
+++ src/js/pages/offer.js
@@ -1,4 +1,4 @@
-const categoryItems = document.querySelectorAll('.category');
+// const categoryItems = document.querySelectorAll('.category');
 
 const rotate3dElement = function (e, target, maxDegrees, scale) {
 	const mouse = {
@@ -29,3 +29,28 @@ const rotate3dElement = function (e, target, maxDegrees, scale) {
 // 		cat.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)';
 // 	});
 // });
+
+// ! --------------
+
+const categoryContainer = document.querySelector('.categories');
+const categoryItems = categoryContainer.querySelectorAll('.category');
+
+categoryContainer.addEventListener('click', (e) => {
+	const categoryTarget = e.target.closest('.category');
+	if (window.innerWidth < 768) {
+		categoryItems.forEach((cat) => {
+			if (categoryTarget !== cat) {
+				cat.classList.remove('category--active');
+				cat.style.maxHeight = '150px';
+			}
+		});
+
+		if (categoryTarget.classList.contains('category--active')) {
+			categoryTarget.classList.remove('category--active');
+			categoryTarget.style.maxHeight = '150px';
+		} else if (categoryTarget) {
+			categoryTarget.classList.add('category--active');
+			categoryTarget.style.maxHeight = categoryTarget.scrollHeight + 'px';
+		}
+	}
+});

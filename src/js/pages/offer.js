16c75480b01cddc5eca57c308dc7551657b67756
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-03-09 17:16:21 +0100

    Add modal for products with more information about product triggered by button

--- src/js/pages/offer.js
+++ src/js/pages/offer.js
@@ -1,56 +1,81 @@
-// const categoryItems = document.querySelectorAll('.category');
+const categoryAnimationMobile = function () {
+	const categoryContainer = document.querySelector('.categories');
+	const categoryItems = categoryContainer.querySelectorAll('.category');
 
-const rotate3dElement = function (e, target, maxDegrees, scale) {
-	const mouse = {
-		x: e.layerX,
-		y: e.layerY,
-	};
-	const image = {
-		x: target.scrollWidth,
-		y: target.scrollHeight,
-	};
+	categoryContainer.addEventListener('click', (e) => {
+		const categoryTarget = e.target.closest('.category');
+		if (window.innerWidth < 768) {
+			categoryItems.forEach((cat) => {
+				if (categoryTarget !== cat) {
+					cat.classList.remove('category--active');
+					cat.style.maxHeight = '150px';
+				}
+			});
 
-	const calcRotateDEG = (mouseAxis, elAxisDimension, maxDegree) => {
-		return ((mouseAxis / elAxisDimension) * 2 - 1) * maxDegree;
-	};
-
-	target.style.transform = `perspective(800px) rotateX(${calcRotateDEG(
-		mouse.y,
-		image.y,
-		maxDegrees
-	)}deg) rotateY(${-calcRotateDEG(mouse.x, image.x, maxDegrees)}deg) scale3d(${scale}, ${scale}, ${scale})`;
+			if (categoryTarget.classList.contains('category--active')) {
+				categoryTarget.classList.remove('category--active');
+				categoryTarget.style.maxHeight = '150px';
+			} else if (categoryTarget) {
+				categoryTarget.classList.add('category--active');
+				categoryTarget.style.maxHeight = categoryTarget.scrollHeight + 'px';
+			}
+		}
+	});
 };
 
-// categoryItems.forEach(function (cat) {
-// 	cat.addEventListener('mousemove', function (e) {
-// 		rotate3dElement(e, cat, 10, 1.02);
-// 	});
-// 	cat.addEventListener('mouseout', () => {
-// 		cat.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)';
-// 	});
-// });
+categoryAnimationMobile();
 
-// ! --------------
+const productsList = document.querySelector('.products__list');
 
-const categoryContainer = document.querySelector('.categories');
-const categoryItems = categoryContainer.querySelectorAll('.category');
+const modalHTML = `
+<div class="modal-product">
+	<h3 class="modal-product__title">VAPORESSO GEN PT60</h3>
+	<img class="modal-product__image" src="../src/img/offer/products/product--1.jpg" alt="">
 
-categoryContainer.addEventListener('click', (e) => {
-	const categoryTarget = e.target.closest('.category');
-	if (window.innerWidth < 768) {
-		categoryItems.forEach((cat) => {
-			if (categoryTarget !== cat) {
-				cat.classList.remove('category--active');
-				cat.style.maxHeight = '150px';
-			}
-		});
+	<div class="modal-product__desc">
+		<div class="modal-product__desc-row">
+			<h4 class="modal-product__desc-title">OPIS</h4>
+			<p class="modal-product__desc-short">Zestaw Max100 producenta Geekvape w kolorze czarno - czerwonym.
+				W
+				skład zestawu wchodzi zasialnie Max100 oraz atomizer Geekvape Z.</p>
+		</div>
+		<div class="modal-product__desc-row">
+			<h4 class="modal-product__desc-title">SPECYFIKACJA</h4>
+			<ul class="modal-product__desc-specification">
+				<li>Regulacja mocy w zakresie 5-100W</li>
+				<li>Kompatybilność z akumulatorami 18650 oraz 21700</li>
+				<li>Kolorowy wyświetlacz TFT o przekątnej 1.08"</li>
+				<li>Certyfikat IP68 (odporność na wodę, kurz oraz wstrząsy)</li>
+				<li>Port USB-C</li>
+			</ul>
+		</div>
+	</div>
+	<button class="modal-product__close-btn modal-product__close-btn--top"><i
+			class="fa-solid fa-xmark"></i></button>
+	<button class="modal-product__close-btn modal-product__close-btn--bottom">Wróć do listy produktów</button>
 
-		if (categoryTarget.classList.contains('category--active')) {
-			categoryTarget.classList.remove('category--active');
-			categoryTarget.style.maxHeight = '150px';
-		} else if (categoryTarget) {
-			categoryTarget.classList.add('category--active');
-			categoryTarget.style.maxHeight = categoryTarget.scrollHeight + 'px';
-		}
+</div>`;
+
+const displayProductModal = function (e) {
+	const product = e.target.closest('.product');
+
+	if (e.target.classList.contains('product__btn')) {
+		const modalContainer = document.createElement('div');
+		modalContainer.classList.add('modal-container');
+
+		modalContainer.innerHTML = modalHTML;
+		document.body.append(modalContainer);
+
+		document.body.style.overflow = 'hidden';
+
+		const closeBtns = modalContainer.querySelectorAll('.modal-product__close-btn');
+		closeBtns.forEach((btn) =>
+			btn.addEventListener('click', () => {
+				modalContainer.remove();
+				document.body.style.overflow = 'auto';
+			})
+		);
 	}
-});
+};
+
+productsList.addEventListener('click', displayProductModal);

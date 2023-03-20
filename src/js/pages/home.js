f13d89cdc2510335a556e6f3124e93f2a5c6ca77
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-03-20 21:04:52 +0100

    Change video

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,11 +1,11 @@
 // ! Background Video
 const headerVideoEl = document.querySelector('.header__video-c video');
 
-headerVideoEl.playbackRate = 0.8;
+headerVideoEl.playbackRate = 0.7;
 
 // Change video quality depends of screen size
-const videoQuality = window.screen.height > 1080 ? '1080' : window.screen.height > 720 ? '720' : '';
-headerVideoEl.src = `./src/video/header-${videoQuality}p.mp4`;
+// const videoQuality = window.screen.height > 1080 ? '1080' : window.screen.height > 720 ? '720' : '';
+// headerVideoEl.src = `./src/video/header-${videoQuality}p.mp4`;
 
 // ! Aniated logo in section about us
 

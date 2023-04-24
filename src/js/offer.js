88dfafe5783f0720dfa5120d6fd9b2c5239cff41
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2023-04-24 14:41:37 +0200

    OFFER / Products - Add content (without images) for 'Plyny' category & Add RWD fix for page title and category slider

--- src/js/offer.js
+++ src/js/offer.js
@@ -88,21 +88,24 @@ const mobileScreen = window.innerWidth < 768;
 const categorySlider = function () {
   const categoryContainer = document.querySelector('.categories__content');
   const categoryItems = categoryContainer.querySelectorAll('.category');
-  const categoryBarHeight = 125;
+  const categoryBarHeight = 150;
 
   // Init for desktop
-  if (mobileScreen)
-    categoryItems[0].style.maxHeight = `${categoryItems[0].scrollHeight}px`;
+  // if (mobileScreen)
+  //   categoryItems[0].style.maxHeight = `${categoryItems[0].scrollHeight}px`;
+
+  const removeActiveClass = function () {
+    categoryItems.forEach((cat) => {
+      cat.classList.remove('category--active');
+      // eslint-disable-next-line no-param-reassign
+      if (mobileScreen) cat.style.maxHeight = `${categoryBarHeight}px`;
+    });
+  };
 
   categoryContainer.addEventListener('click', (e) => {
     const categoryTarget = e.target.closest('.category');
-    categoryItems.forEach((cat) => {
-      if (categoryTarget !== cat) {
-        cat.classList.remove('category--active');
-        // eslint-disable-next-line no-param-reassign
-        if (mobileScreen) cat.style.maxHeight = `${categoryBarHeight}px`;
-      }
-    });
+
+    removeActiveClass();
 
     if (categoryTarget.classList.contains('category--active') && mobileScreen) {
       categoryTarget.style.maxHeight = `${categoryBarHeight}px`;
@@ -111,6 +114,12 @@ const categorySlider = function () {
       categoryTarget.classList.add('category--active');
       if (mobileScreen)
         categoryTarget.style.maxHeight = `${categoryTarget.scrollHeight}px`;
+    }
+  });
+
+  document.body.addEventListener('click', (e) => {
+    if (!e.target?.closest('.categories__content')) {
+      removeActiveClass();
     }
   });
 };
@@ -144,69 +153,6 @@ const scrollToCategoryOfProducts = function () {
 };
 
 // ----------------------------------------------------------
-// TODO not completed
-
-const displayProductCard = function () {
-  // Logic to implement
-  // 1. Add listener for all products - on click
-  // 2. Creata database for all products which contains all data for each product
-  // 3. Click on product creates a modal by using product ID and matching it in products database
-
-  const productsList = document.querySelector('.products__list');
-
-  const modalHTML = `
-  <div class="modal-product">
-    <h3 class="modal-product__title">VAPORESSO GEN PT60</h3>
-    <img class="modal-product__image" src="../src/img/offer/products/product--1.jpg" alt="">
-  
-  <div class="modal-product__desc">
-  <div class="modal-product__desc-row">
-  <h4 class="modal-product__desc-title">OPIS</h4>
-  <p class="modal-product__desc-short">Zestaw Max100 producenta Geekvape w kolorze czarno - czerwonym. W skład zestawu wchodzi zasialnie Max100 oraz atomizer Geekvape Z.</p>
-  </div>
-  <div class="modal-product__desc-row">
-  <h4 class="modal-product__desc-title">SPECYFIKACJA</h4>
-  <ul class="modal-product__desc-specification">
-  <li>Regulacja mocy w zakresie 5-100W</li>
-  <li>Kompatybilność z akumulatorami 18650 oraz 21700</li>
-  <li>Kolorowy wyświetlacz TFT o przekątnej 1.08"</li>
-  <li>Certyfikat IP68 (odporność na wodę, kurz oraz wstrząsy)</li>
-  <li>Port USB-C</li>
-  </ul>
-  </div>
-  </div>
-  <button class="modal-product__close-btn modal-product__close-btn--top"><i
-  class="fa-solid fa-xmark"></i></button>
-  <button class="modal-product__close-btn modal-product__close-btn--bottom">Wróć do listy produktów</button>
-  
-  </div>`;
-
-  const displayProductModal = function (e) {
-    // const product = e.target.closest('.product');
-
-    if (e.target.classList.contains('product__btn')) {
-      const modalContainer = document.createElement('div');
-      modalContainer.classList.add('modal-container');
-
-      modalContainer.innerHTML = modalHTML;
-      document.body.append(modalContainer);
-
-      document.body.style.overflow = 'hidden';
-
-      const closeBtns = modalContainer.querySelectorAll(
-        '.modal-product__close-btn'
-      );
-      closeBtns.forEach((btn) =>
-        btn.addEventListener('click', () => {
-          modalContainer.remove();
-          document.body.style.overflow = 'auto';
-        })
-      );
-    }
-  };
-
-  productsList.addEventListener('click', displayProductModal);
-};
 
 // Main functions
 mobileNavigationHandler();
@@ -216,4 +162,3 @@ scrollToTop();
 // Functions for this particural page
 categorySlider();
 scrollToCategoryOfProducts();
-// displayProductCard();
